d9753a48b1523348ff3270a823cb301472b52854
forever <ever_311@hotmail.com>
2022-11-22 20:43:56 -0500

    modificado

--- cargo.php
+++ cargo.php
@@ -40,7 +40,7 @@ include "include/verificar_sesion.php";
               <div class="col-md-12 col-sm-12 col-xs-12">
                 <div class="x_panel">
                   <div class="x_title">
-                    <h2>Programa de Estudios</h2>
+                    <h2>Cargo</h2>
                     
                     <div class="clearfix"></div>
                   </div>
--- periodo_academico.php
+++ periodo_academico.php
@@ -40,7 +40,7 @@ include "include/verificar_sesion.php";
               <div class="col-md-12 col-sm-12 col-xs-12">
                 <div class="x_panel">
                   <div class="x_title">
-                    <h2>Registro de Estudiante</h2>
+                    <h2>Registro periodo academico</h2>
                     
                     <div class="clearfix"></div>
                   </div>
